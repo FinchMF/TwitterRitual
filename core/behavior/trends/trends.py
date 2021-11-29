39f768fc9f5456540dc1c39644cb454a12bab173
Matthew Finch <finchrmatthew@gmail.com>
2021-11-28 20:47:33 -0500

    added trends module

---
+++ core/behavior/trends/trends.py
@@ -0,0 +1,13 @@
+"""
+Contains process for Trends to be searched
+"""
+
+
+class Trend(object):
+
+    def __init__(self, TwitterBot: object):
+
+        self.__bot: object = TwitterBot
+        
+
+    
